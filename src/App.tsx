0c4aea1848eea9c0e66624e66c1398357d177e8c
Rajdeep Roy Chowdhury <rrajdeeproychowdhury@gmail.com>
2023-05-24 09:41:16 +0530

    feat: code added to fetch response from actual backend

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,17 @@ import { useState } from 'react'
 import './App.css'
 import AutocompleteBox from './components/AutocompleteBox'
 import { Input, Stack } from '@mui/material'
+import fetchData from './services/LowesService'
+import AutocompleteTerm from './model/AutocompleteTerm'
 
 function App() {
 
   const [userQuery, setUserQuery] = useState<string>('')
+  const [autocompleteTerms, setAutocompleteTerms] = useState<AutocompleteTerm[]>([])
 
   const handleUserQueryChange = (e: any) => {
     setUserQuery(e.target.value)
+    fetchData(e.target.value)
   }
 
   return (
---
+++ src/services/LowesService.ts
@@ -0,0 +1,30 @@
+import AutocompleteTerm from "../model/AutocompleteTerm"
+
+const baseUrl = 'https://www.lowes.com'
+const path = 'LowesSearchServices/resources/autocomplete/v2_0'
+
+const fetchData = (searchTerm: string): AutocompleteTerm[] => {
+    const parameters = `searchTerm=${searchTerm}&maxTerms=8`
+    const completeUrl = `${baseUrl}/${path}?${parameters}`
+
+    const result: AutocompleteTerm[] = []
+
+    fetch(completeUrl, {
+        headers: {
+            'Accept': 'application/json',
+            'Access-Control-Allow-Origin': '*',
+        }
+    }).then(data => {
+        const dataJson = data.json()
+        console.log(dataJson)
+        // dataJson['terms'].map(term => {
+        //     const autocompleteTerm = new AutocompleteTerm()
+        //     autocompleteTerm.name = term.name
+        //     result.push(autocompleteTerm)
+        // })
+    })
+
+    return result
+}
+
+export default fetchData;
